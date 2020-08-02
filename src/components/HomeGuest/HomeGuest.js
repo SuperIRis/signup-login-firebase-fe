d278852108e30f8d06053f9b5be5095541d06893
Iris <iris@element.com.mx>
2020-08-02 22:21:51 +0200

    Testing pipelines

--- src/components/HomeGuest/HomeGuest.js
+++ src/components/HomeGuest/HomeGuest.js
@@ -2,26 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-const HomeGuest = ()=>{
+const HomeGuest = () => {
   return (
     <div>
-      <h1>Hello stranger!</h1>
+      <h1>Hello stranger! Test</h1>
       <ul>
         <li>
-          <Link to="/login">Login</Link>
+          <Link to='/login'>Login</Link>
         </li>
         <li>
-          <Link to="/signup">Signup</Link>
+          <Link to='/signup'>Signup</Link>
         </li>
       </ul>
     </div>
   );
-}
+};
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
-    data:state
-  }
+    data: state,
+  };
 }
 
 export default connect(mapStateToProps)(HomeGuest);
