611b36816437fd9ce49ab3d4c1d73986f51d9f23
Iris <iris@element.com.mx>
2020-08-05 23:36:20 +0200

    Testing firebase configuration

--- middleware/setup.js
+++ middleware/setup.js
@@ -9,7 +9,7 @@ export function getProviderConfig() {
   if (getProvider() === FIREBASE) {
     if (process.env.BUILD_TARGET && process.env.BUILD_TARGET === 'server') {
       const functions = require('firebase-functions');
-      providerConfig = functions.config() ? functions.config : {};
+      providerConfig = functions.config() ? functions.config() : {};
     } else if (!providerConfig) {
       providerConfig = window.__PROVIDER_CONFIG__;
       //delete window.__PROVIDER_CONFIG__;
