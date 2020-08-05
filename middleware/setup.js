a52f7a8d20728a25ec37dcfcea56503f0decb69b
Iris <iris@element.com.mx>
2020-08-05 22:50:07 +0200

    Fixing tests

--- middleware/setup.js
+++ middleware/setup.js
@@ -1,5 +1,5 @@
 export const FIREBASE = 'FIREBASE';
-let providerConfig;
+let providerConfig = {};
 
 export function getProvider() {
   return FIREBASE;
@@ -9,15 +9,11 @@ export function getProviderConfig() {
   if (getProvider() === FIREBASE) {
     if (process.env.BUILD_TARGET && process.env.BUILD_TARGET === 'server') {
       const functions = require('firebase-functions');
-      console.log('functions', functions.config());
-      console.log('GCLOUD', process.env.GCLOUD_PROJECT);
-      return functions.config() || {};
-    } else {
-      if (!providerConfig) {
-        providerConfig = window.__PROVIDER_CONFIG__;
-        //delete window.__PROVIDER_CONFIG__;
-      }
-      return providerConfig;
+      providerConfig = functions.config() ? functions.config : {};
+    } else if (!providerConfig) {
+      providerConfig = window.__PROVIDER_CONFIG__;
+      //delete window.__PROVIDER_CONFIG__;
     }
   }
+  return providerConfig;
 }
