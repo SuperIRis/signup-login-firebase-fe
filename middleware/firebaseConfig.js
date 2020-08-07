a118147c27102df832a69700440b4b3a4e168e3e
Iris <iris@element.com.mx>
2020-08-08 00:25:32 +0200

    Testing firebase env

--- middleware/firebaseConfig.js
+++ middleware/firebaseConfig.js
@@ -1,4 +1,5 @@
 import { getProviderConfig, getProvider } from '../middleware/setup';
 export function getFBAppId() {
+  console.log('provider config', getProviderConfig());
   return getProviderConfig().facebook ? getProviderConfig().facebook.appid : 'NOPE';
 }
