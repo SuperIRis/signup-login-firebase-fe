22a4d636c642e0cf89dd3746609718027d11d51e
Iris <iris@element.com.mx>
2021-01-03 22:03:41 +0100

    Updated sagas tests, still some pending

--- src/sagas/sagas.test.js
+++ src/sagas/sagas.test.js
@@ -1,15 +1,27 @@
-import { SIGNUP_REQUEST, LOGIN_REQUEST } from '../actions/constants';
+import { SIGNUP_REQUEST, LOGIN_REQUEST, LOGOUT_REQUEST, VERIFY_USER_FOR_SIGNUP_REQUEST } from '../actions/constants';
 import { setAuthState, sendingRequest, setError } from '../actions/actions';
-import sagasRoot, { loginFlow, signupFlow, authorize } from './sagas';
+import sagasRoot from './sagas';
+import {
+  loginFlow,
+  signupFlow,
+  logoutFlow,
+  verifyUserForSignupFlow,
+  recoverPasswordFlow,
+  verifyResetPasswordRequestFlow,
+  authorize,
+} from './auth';
+import { resetPasswordFlow } from './account';
 import { take, call, put, fork } from 'redux-saga/effects';
 import auth from '../models/auth';
 describe('Sagas', () => {
   const user = { username: 'superiris', password: 'Admin123' };
   const data = { ...user, loginAttempts: 1 };
   const putLogged = { ...put(setAuthState(true)) };
+  const putUnlogged = { ...put(setAuthState(false)) };
+  const putVerifiedForSignup = { type: VERIFY_USER_FOR_SIGNUP_REQUEST, verified: true, user };
   const sendRequestTrue = { ...put(sendingRequest(true)) };
   const sendRequestFalse = { ...put(sendingRequest(false)) };
-  const sendError = { ...put(setError(new Error('authType not valid:'))) };
+  const sendError = { ...put(setError({ message: 'authType not valid:' })) };
   it('Succesfully go through loginFlow ', () => {
     const gen = loginFlow();
     expect(gen.next().value).toEqual(take('LOGIN_REQUEST'));
@@ -24,6 +36,21 @@ describe('Sagas', () => {
     });
     expect(gen.next(true).value).toEqual(putLogged);
   });
+  it('Succesfully go through logoutFlow ', () => {
+    const gen = logoutFlow();
+    expect(gen.next().value).toEqual(take('LOGOUT_REQUEST'));
+    expect(gen.next({}).value).toEqual({
+      ...call(authorize, {}, LOGOUT_REQUEST),
+    });
+    expect(gen.next(true).value).toEqual(putUnlogged);
+  });
+  it('Succesfully go through verifyUserForSignupFlow', () => {
+    const gen = verifyUserForSignupFlow();
+    expect(gen.next().value).toEqual(take(VERIFY_USER_FOR_SIGNUP_REQUEST));
+    expect(gen.next({}).value).toEqual({
+      ...call(authorize, undefined, VERIFY_USER_FOR_SIGNUP_REQUEST, undefined),
+    });
+  });
   it('Sends LOGIN_REQUEST in authorize for logins ', () => {
     const gen = authorize({}, LOGIN_REQUEST);
     expect(gen.next().value).toEqual(sendRequestTrue);
@@ -36,18 +63,20 @@ describe('Sagas', () => {
     expect(gen.next().value).toEqual({ ...call(auth.signup, {}, undefined) });
     expect(gen.next().value).toEqual(sendRequestFalse);
   });
-  it('Throws an error when user is not authorized ', () => {
-    const mockFn = jest.fn(() => {
-      throw 'Error';
-    });
+  it('Throws an error when authType is not defined', () => {
     const gen = authorize();
     expect(gen.next().value).toEqual(sendRequestTrue);
-    expect(gen.next().value).toEqual(sendError);
+    expect(gen.next().value.message).toEqual(sendError.message);
     expect(gen.next().value).toEqual(sendRequestFalse);
   });
-  it('Root listens to signup and login', () => {
+  it('Root listens to all auth and account sagas', () => {
     const gen = sagasRoot();
     expect(gen.next().value).toEqual(fork(signupFlow));
     expect(gen.next().value).toEqual(fork(loginFlow));
+    expect(gen.next().value).toEqual(fork(logoutFlow));
+    expect(gen.next().value).toEqual(fork(verifyUserForSignupFlow));
+    expect(gen.next().value).toEqual(fork(recoverPasswordFlow));
+    expect(gen.next().value).toEqual(fork(verifyResetPasswordRequestFlow));
+    expect(gen.next().value).toEqual(fork(resetPasswordFlow));
   });
 });
